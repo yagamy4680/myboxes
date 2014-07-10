3ae072e4e4d53c1061798cf7cfb58c45bbcb7221
yagamy4680 <yagamy@gmail.com>
2014-07-10 20:09:11 +0800

    script for installing usbip module and tools on Ubuntu from kernel sources

---
+++ scripts/install_usbip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+LINUX_VERSION=$(uname -r | cut -d'-' -f1)
+
+apt-get install -y autoconf build-essential libtool libsysfs-dev
+apt-get install -y linux-source
+tar -xjf /usr/src/linux-source-${LINUX_VERSION}.tar.bz2
+cd linux-source-${LINUX_VERSION}/drivers/staging/usbip/userspace/
+./autogen.sh
+./configure --prefix=/usr --sysconfdir=/etc
+make
+make install
+
+AUTO_MODULE="/etc/modules"
+
+modprobe -a usbip-core usbip-host vhci-hcd
+sed -i 's/^usbip-core//g' ${AUTO_MODULE}
+sed -i 's/^usbip-host//g' ${AUTO_MODULE}
+sed -i 's/^vhci-hcd//g' ${AUTO_MODULE}
+echo usbip-core >> ${AUTO_MODULE}
+echo usbip-host >> ${AUTO_MODULE}
+echo vhci-hcd >> ${AUTO_MODULE}
+
+echo "please type 'usbipd -D' to launch the USB/IP daemon ..."
+
