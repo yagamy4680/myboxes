4095504b133a04030b480d69b6e54478f73e6cca
yagamy4680 <yagamy@gmail.com>
2014-07-12 00:20:37 +0800

    Add usb/ip kernel module and daemon for Arduino development

--- scripts/install_usbip.sh
+++ scripts/install_usbip.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 LINUX_VERSION=$(uname -r | cut -d'-' -f1)
 
+MARK="/opt/usbip.installation.mark"
+
+if [ -f "${MARK}" ]; then
+	echo "usbip kernel module is installed"
+fi
+
 apt-get install -y autoconf build-essential libtool libsysfs-dev
 apt-get install -y linux-source
+cd /tmp
 tar -xjf /usr/src/linux-source-${LINUX_VERSION}.tar.bz2
 cd linux-source-${LINUX_VERSION}/drivers/staging/usbip/userspace/
 ./autogen.sh
