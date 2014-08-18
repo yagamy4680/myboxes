c717fafa0249f70200c679cf759ed84002676a39
yagamy <yagamy@gmail.com>
2014-08-18 09:59:02 +0800

    update arduino box for bluez development

--- scripts/init_arduino.sh
+++ scripts/init_arduino.sh
@@ -10,3 +10,8 @@ apt-get install -y arduino python-pip picocom build-essential vim
 # Install command-line tools for Arduino build system
 #
 pip install ino
+
+# Install all necessary packages for BlueZ development
+#
+apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+apt-get install -y python-gobject python-dbus
