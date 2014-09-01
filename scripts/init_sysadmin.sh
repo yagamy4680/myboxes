e8a6fb770ff783b5a68fd9926a4d9cd79c2bd956
yagamy4680 <yagamy@gmail.com>
2014-09-01 19:37:18 +0800

    add sysadmin box

---
+++ scripts/init_sysadmin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "udpate apt repository"
+apt-get update -q
+
+echo "install required system packages"
+apt-get install -y vim virt-what
+
+echo "install docker"
+curl -s https://get.docker.io/ubuntu/ | sudo sh
