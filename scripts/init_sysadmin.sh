2bb8d7f22d55483be6ebd511f8e5cbd631a58a5b
yagamy4680 <yagamy@gmail.com>
2014-09-01 19:50:17 +0800

    add GIT for sysadmin box

--- scripts/init_sysadmin.sh
+++ scripts/init_sysadmin.sh
@@ -4,7 +4,7 @@ echo "udpate apt repository"
 apt-get update -q
 
 echo "install required system packages"
-apt-get install -y vim virt-what
+apt-get install -y vim virt-what git
 
 echo "install docker"
 curl -s https://get.docker.io/ubuntu/ | sudo sh
