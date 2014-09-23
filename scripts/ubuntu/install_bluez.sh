0b50111d9f178c8912002d601bfa9fdf0959b4f0
yagamy4680 <yagamy@gmail.com>
2014-09-23 10:34:45 +0800

    fix bluez installation script

--- scripts/ubuntu/install_bluez.sh
+++ scripts/ubuntu/install_bluez.sh
@@ -2,7 +2,7 @@
 [ "true" == "${IGNORE_REPO_UPDATE}" ] || apt-get udpate
 
 # Install all necessary packages for building BlueZ
-apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev build-essential git
+apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev build-essential git socat
 
 # Build BlueZ and install.
 cd /opt
@@ -10,13 +10,13 @@ git clone https://github.com/yagamy4680/bluepy.git
 cd bluepy
 BLUEZ_INSTALLATION=true ./build.sh
 
+
 # With py-gobject and dbus modules, the bluez python sample
 # codes can work normally.
-# 
+#
 if [ "true" == "${INSTALL_PYTHON_PACKAGES}" ]; then
-	apt-get install -y python-pip
-	# pip install python-gobject python-dbus pyserial
-    pip install pygobject python-dbus pyserial
+	apt-get install -y python-pip python-dev python-gobject python-dbus
+  pip install pyserial rainbow_logging_handler
 fi
 
 
