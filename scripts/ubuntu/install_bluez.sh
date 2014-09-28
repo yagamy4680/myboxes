1132eccb1460aebd1adc6cdfe810ec3615172f27
yagamy <yagamy@gmail.com>
2014-09-29 01:33:06 +0800

    temporarily disable noble/bleno/sensortag nodejs package installations due to unknown hang

--- scripts/ubuntu/install_bluez.sh
+++ scripts/ubuntu/install_bluez.sh
@@ -2,7 +2,7 @@
 [ "true" == "${IGNORE_REPO_UPDATE}" ] || apt-get udpate
 
 # Install all necessary packages for building BlueZ
-apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev build-essential git socat
+apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev build-essential git socat libbluetooth-dev
 
 # Build BlueZ and install.
 cd /opt
@@ -16,14 +16,16 @@ BLUEZ_INSTALLATION=true ./build.sh
 #
 if [ "true" == "${INSTALL_PYTHON_PACKAGES}" ]; then
 	apt-get install -y python-pip python-dev python-gobject python-dbus
-  pip install pyserial rainbow_logging_handler
+	pip install pyserial rainbow_logging_handler
 fi
 
 
-#
+
 if [ "true" == "${INSTALL_NODEJS_PACKAGES}" ]; then
-	apt-get install -y libbluetooth-dev
-	npm install -g noble
-	npm install -g bleno
-	npm install -g sensortag
+# Sometimes, install noble/bleno/sensortag on VirtualBox might hang. Let's install
+# these packages manually
+# 
+#	npm install -g noble
+#	npm install -g bleno
+#	npm install -g sensortag
 fi
